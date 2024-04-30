6313aa059f4ace26904267a92b993e8b2534fc8b
chenshi <cshi@miuandes.cl>
2024-04-30 19:18:23 -0400

    datos extraidos

--- tarea2.py
+++ tarea2.py
@@ -1,9 +1,68 @@
-def abrir_archivo(archivo):
-    with open(archivo, 'r') as file:
-        archivo = file.read()
-    return archivo
 
-def identificar_estados()
+with open('nfa4.txt', 'r') as file:
+    automata = file.readlines()
+########################################EXTRACCIÓN DE DATOS##################################################
+estados = []
+estado_inicial = None
+estados_finales = []
+alfabeto = []
+copia_automata = [line for line in automata]
+transiciones = []
 
-abrir = input("Indique el archivo que contiene al automata: ")
-print(abrir_archivo(abrir))
+for line in automata:
+    if line == automata[0]:
+        copia_automata.remove(line)
+        continue
+    if line[0] == ">":
+        estado_inicial = line[1]
+        estados.append(line[1])
+    elif line[0] == "*":
+        estados_finales.append(line[1])
+        estados.append(line[1])
+    elif line == "Alfabeto\n":
+        copia_automata.remove(line)
+        automata = [line for line in copia_automata]
+        break
+    else: 
+        estados.append(line[0])
+    copia_automata.remove(line)
+
+for line in automata:
+    if line == 'Transiciones\n':
+        copia_automata.remove(line)
+        automata = [line for line in copia_automata]
+        break
+    alfabeto.append(line[0])
+    copia_automata.remove(line)
+
+for line in automata:
+    transiciones.append(((line[0], line[2]), line[7]))
+
+# print(alfabeto)
+# print(estado_inicial)
+# print(estados)
+# print(estados_finales)
+# print(transiciones)
+###########################################FORMAR LOS ESTADOS FINALES####################################################
+estados_finales = []
+
+for camino in alfabeto:
+    ...
+
+# for transicion_1 in transiciones:
+#     estado=[]
+#     if transicion_1[1] not in estado:
+#         estado.append(transicion_1[1])
+
+#     for transicion_2 in transiciones:
+#         if transicion_1[0] == transicion_2[0] and (transicion_2[1] not in estado):
+#             estado.append(transicion_2[1])
+
+#     if estado not in estados_finales:
+#         estados_finales.append(estado)
+
+# print(estados_finales)
+
+
+    
+
---
+++ testing.py
@@ -0,0 +1,4 @@
+hola= ["hola", "como", "estas"]
+hola_copia = hola
+hola.remove("hola")
+print(hola_copia)
